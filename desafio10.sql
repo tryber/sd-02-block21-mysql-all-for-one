2ce69a2bad552b8f3e17e14dae39a1372acd5e3c
Andrey <andreytsuzuki@gmail.com>
2020-05-11 17:41:00 -0300

    sql

---
+++ desafio10.sql
@@ -0,0 +1,3 @@
+select * from northwind.purchase_orders 
+where created_by >= 2
+order by created_by desc;
---
+++ desafio12.sql
@@ -0,0 +1,2 @@
+select supplier_id from northwind.purchase_orders
+where submitted_date like '2006-01-14%';
