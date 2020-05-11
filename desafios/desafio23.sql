dbc2c335ea2625ea64f6dc0fb32908d6f1482d1d
richardwellerson <richardwellerson@gmail.com>
2020-05-11 18:24:09 -0300

    Correçao do desafio

--- desafios/desafio23.sql
+++ desafios/desafio23.sql
@@ -1,1 +1,1 @@
-update northwind.order_details set discount = 30 where unit_price > '10.0000' and (id between 30 and 40);
+update northwind.order_details set discount = 30 where unit_price < '10.0000';
