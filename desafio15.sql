2d05d1f7c456d351c2c32a5b24bf42681bd91acd
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-05-11 16:55:31 -0300

    Desafios sobre filtragem de dados

---
+++ desafio15.sql
@@ -0,0 +1,2 @@
+SELECT DATE_FORMAT(submitted_date, '%H:%i:%s') 
+AS 'submitted_date' FROM northwind.purchase_orders;
---
+++ desafio19.sql
@@ -0,0 +1,3 @@
+SELECT * from orders
+WHERE employee_id IN (5, 6)
+AND shipper_id = 2;
