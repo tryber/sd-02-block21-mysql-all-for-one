e29eb5a36dd45169cd543e664e6b6a9b59cefa19
eddyeboy27 <lipe0503@gmail.com>
2020-05-14 23:03:02 -0300

    Correção uppercase req 19

--- requisitos/desafio19.sql
+++ requisitos/desafio19.sql
@@ -1,5 +1,5 @@
 # Requisito 19 - Quantos pedidos foram feitos na tabela orders pelo employee_id igual a 6 ou 5, e que foram enviados através do método shipper_id = 2 ?
 
 SELECT * FROM northwind.orders
-WHERE employee_id in(6, 5)
+WHERE employee_id IN(6, 5)
 AND shipper_id = 2;
