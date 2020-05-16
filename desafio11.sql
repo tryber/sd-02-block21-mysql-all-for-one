631ad9a3e65e22e05af66f3ac6ebcfb4377144cf
phtf92 <phtf92@gmail.com>
2020-05-16 16:53:09 -0300

    Desafios 5, 11, 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,2 @@
-SELECT * from northwind.purchase_orders
-WHERE notes BETWEEN 'Purchase generated based on Order #3%' AND 'Purchase generated based on Order #39';
+SELECT notes from northwind.purchase_orders
+WHERE notes like '%#3_';
--- desafio12.sql
+++ desafio12.sql
@@ -1,2 +1,3 @@
 SELECT submitted_date from northwind.purchase_orders
-WHERE submitted_date = '2006/01/14';
+WHERE submitted_date BETWEEN '2006/01/14 00:00:00' AND '2006/01/14 23:59:59';
+
